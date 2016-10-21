4c94d51b1e175562553baf0516c59a7c15deb2c6
Jose Luis Toxtle Ocotoxtle <drabat199@gmail.com>
2016-10-21 18:30:43 -0500

    Agrego SplashScreen e Imagenes

--- app/src/main/java/com/devtx/drabatx/fifapp/SplashScreenActivity.java
+++ app/src/main/java/com/devtx/drabatx/fifapp/SplashScreenActivity.java
@@ -1,13 +1,24 @@
 package com.devtx.drabatx.fifapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity implements Runnable{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        /*Se cambia de activity tras transcurrir 3 segundos*/
+        Handler handler = new Handler();
+        handler.postDelayed(this,3000);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
